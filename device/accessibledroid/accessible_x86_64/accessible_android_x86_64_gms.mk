3fa8ad6bb1ccc156b112cac489eff344630fd1ec
ejjjkkjlkkj <ism3pro@outlook.com>
2026-09-10 21:56:29 +0200

    Fix GMS edition identity and product inheritance

--- device/accessibledroid/accessible_x86_64/accessible_android_x86_64_gms.mk
+++ device/accessibledroid/accessible_x86_64/accessible_android_x86_64_gms.mk
@@ -1,13 +1,13 @@
 # Accessible Android x86_64 product with an externally supplied authorized GMS layer.
 
-$(call inherit-product, device/accessibledroid/accessible_x86_64/accessible_android_x86_64.mk)
+$(call inherit-product, device/accessibledroid/accessible_x86_64/accessible_android_x86_64_base.mk)
 $(call inherit-product, vendor/accessibledroid/private-gms/gms-product.mk)
 
 PRODUCT_NAME := accessible_android_x86_64_gms
-PRODUCT_DEVICE := accessible_x86_64
-PRODUCT_BRAND := AccessibleAndroid
 PRODUCT_MODEL := Accessible Android 17 GMS x86_64 VM
-PRODUCT_MANUFACTURER := Accessible Android Project
+
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.vendor.accessibledroid.edition=gms
 
 # gms-product.mk owns the proprietary package declarations. This repository
 # deliberately contains no Google application binaries.
