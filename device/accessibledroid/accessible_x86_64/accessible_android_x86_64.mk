92f82395a933305e4c21330d4c5b500b36364620
ejjjkkjlkkj <ism3pro@outlook.com>
2026-09-10 21:56:24 +0200

    Fix clean AOSP edition product inheritance

--- device/accessibledroid/accessible_x86_64/accessible_android_x86_64.mk
+++ device/accessibledroid/accessible_x86_64/accessible_android_x86_64.mk
@@ -1,14 +1,9 @@
-# Base Accessible Android x86_64 product.
+# Open Accessible Android x86_64 product.
 
-$(call inherit-product, device/generic/common/x86_64.mk)
-$(call inherit-product-if-exists, vendor/accessibledroid/product/accessibility.mk)
+$(call inherit-product, device/accessibledroid/accessible_x86_64/accessible_android_x86_64_base.mk)
 
 PRODUCT_NAME := accessible_android_x86_64
-PRODUCT_DEVICE := accessible_x86_64
-PRODUCT_BRAND := AccessibleAndroid
 PRODUCT_MODEL := Accessible Android 17 x86_64 VM
-PRODUCT_MANUFACTURER := Accessible Android Project
 
 PRODUCT_VENDOR_PROPERTIES += \
-    ro.vendor.accessibledroid.edition=aosp \
-    ro.vendor.accessibledroid.vm_reference=qemu
+    ro.vendor.accessibledroid.edition=aosp
