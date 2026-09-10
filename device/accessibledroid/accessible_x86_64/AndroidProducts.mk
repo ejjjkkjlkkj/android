a3cd87c053b5e8afa569b36a9bf9012d3b199875
ejjjkkjlkkj <ism3pro@outlook.com>
2026-09-10 22:41:29 +0200

    Modernize AccessibleAndroid lunch targets for Android 17

--- device/accessibledroid/accessible_x86_64/AndroidProducts.mk
+++ device/accessibledroid/accessible_x86_64/AndroidProducts.mk
@@ -3,7 +3,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/accessible_android_x86_64_gms.mk
 
 COMMON_LUNCH_CHOICES := \
-    accessible_android_x86_64-userdebug \
-    accessible_android_x86_64-user \
-    accessible_android_x86_64_gms-userdebug \
-    accessible_android_x86_64_gms-user
+    accessible_android_x86_64-aosp_current-userdebug \
+    accessible_android_x86_64-aosp_current-user \
+    accessible_android_x86_64_gms-aosp_current-userdebug \
+    accessible_android_x86_64_gms-aosp_current-user
