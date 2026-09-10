abbfb99b73aa08454ab65ac49e3afd25c7122ae2
ejjjkkjlkkj <ism3pro@outlook.com>
2026-09-10 23:33:20 +0200

    Install Android serial boot milestone init script

--- device/accessibledroid/accessible_x86_64/accessible_android_x86_64_base.mk
+++ device/accessibledroid/accessible_x86_64/accessible_android_x86_64_base.mk
@@ -27,7 +27,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # diagnostics and recovery tooling after the real vendor partition is mounted.
 PRODUCT_COPY_FILES += \
     device/accessibledroid/accessible_x86_64/fstab.accessible_x86_64:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.accessible_x86_64 \
-    device/accessibledroid/accessible_x86_64/fstab.accessible_x86_64:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.accessible_x86_64
+    device/accessibledroid/accessible_x86_64/fstab.accessible_x86_64:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.accessible_x86_64 \
+    device/accessibledroid/accessible_x86_64/init.accessibledroid.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.accessibledroid.rc
 
 # Accessibility is part of the base system and is independent of GMS.
 $(call inherit-product, vendor/accessibledroid/product/accessibility.mk)
